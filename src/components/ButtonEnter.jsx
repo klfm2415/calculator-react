3399e64f1f88b0bc617be1e5a6dbc054cae7d789
klfm2415 <klfm241500@gmail.com>
2024-05-10 16:32:17 +0900

    ButtonEnter

--- src/components/ButtonEnter.jsx
+++ src/components/ButtonEnter.jsx
@@ -1,5 +1,61 @@
-const ButtonEnter = ({ name }) => {
-  const onClickButton = () => {};
+import Button from "./Button";
 
-  return <Button name={name} />;
+const ButtonEnter = ({ name, operator, result, setResult, temp, setTemp }) => {
+  const onClickButton = () => {
+    switch (operator) {
+      case "+":
+        setTemp(+temp + +result);
+        setResult(+temp + +result);
+        break;
+      case "-":
+        setTemp(+temp - +result);
+        setResult(+temp - +result);
+        break;
+      case "*":
+        setTemp(+temp * +result);
+        setResult(+temp * +result);
+        break;
+      case "/":
+        setTemp(parseInt(+temp / +result, 10));
+        setResult(parseInt(+temp / +result, 10));
+        break;
+      default:
+        alert("잘못된 연산자입니다");
+    }
+  };
+
+  return <Button name={name} onClickButton={onClickButton} />;
 };
+
+export default ButtonEnter;
+
+/*import Button from "./Button";
+
+const ButtonEnter = ({ name, operator, result, setresult, temp, setTemp }) => {
+  const onClickButton = () => {
+    switch (operator) {
+      case "+":
+        setTemp(+temp + +result);
+        setresult(+temp + +result);
+        break;
+      case "-":
+        setTemp(+temp - +result);
+        setresult(+temp - +result);
+        break;
+      case "*":
+        setTemp(+temp * +result);
+        setresult(+temp * +result);
+        break;
+      case "/":
+        setTemp(parseInt(+temp / +result, 10));
+        setresult(parseInt(+temp / +result, 10));
+        break;
+      default:
+        alert("잘못된 연산자입니다");
+    }
+  };
+
+  return <Button name={name} onClickButton={onClickButton} />;
+};
+
+export default ButtonEnter;*/
