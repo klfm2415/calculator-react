2a42fd0ab808f186510e4be76e59351aaeda001d
klfm2415 <klfm241500@gmail.com>
2024-05-10 17:39:20 +0900

    Calculator-re

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ name, onClickButton }) => {
   return (
     <button
-      className="bg-blue-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
+      className="bg-green-200 w-15 h-15 border-radius: 50-px flex justify-center items-center shadow-sm hover"
       onClick={onClickButton}
     >
       {name}
